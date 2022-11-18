cc8c3c67321dbc573c698a69fc666bbd1449f4c2
Mak992 <maksimponomarev992@gmail.com>
2022-11-18 17:45:04 +0300

    Added "Venit" turret with flak bullets

--- src/testj/content/TestjmodBlocks.java
+++ src/testj/content/TestjmodBlocks.java
@@ -1,16 +1,24 @@
 package testj.content;
 
+import arc.graphics.Color;
+import mindustry.content.Fx;
 import mindustry.content.Items;
+import mindustry.entities.bullet.*;
+import mindustry.entities.pattern.*;
+import mindustry.graphics.Pal;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.defense.Wall;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
 
 import static mindustry.type.ItemStack.with;
 
 public class TestjmodBlocks {
     public static Block
             //defense
-            testWall, testWallLarge;
+            testWall, testWallLarge,
+            //turrets
+            venit;
 
     public static void load() {
         int wallHealthMultiplier = 4;
@@ -26,6 +34,62 @@ public class TestjmodBlocks {
                 health = 70 * wallHealthMultiplier * 4;
                 requirements(Category.defense, with(Items.copper, 32));
             }
+
         };
+        //enderegion; region turrets
+                venit = new ItemTurret("venit"){{
+                    requirements(Category.turret, with(Items.lead, 30, Items.graphite, 10));
+                    ammo(
+                            Items.lead, new FlakBulletType(){{
+                                speed = 5;
+                                height = 9;
+                                width = 9;
+                                splashDamage = 18f;
+                                splashDamageRadius = 60f;
+                                backColor = Pal.plastaniumBack;
+                                frontColor = Pal.plastaniumFront;
+                                hitEffect = Fx.flakExplosionBig;
+                                collidesGround = true;
+                            }},
+                            Items.graphite, new FlakBulletType(){{
+                                speed = 4;
+                                height = 10;
+                                width = 10;
+                                splashDamage = 28f;
+                                splashDamageRadius = 42f;
+                                backColor = Pal.bulletYellowBack;
+                                frontColor = Pal.bulletYellow;
+                                hitEffect = Fx.flakExplosionBig;
+                                collidesGround = true;
+                            }},
+                            Items.silicon, new FlakBulletType(){{
+                                speed = 6;
+                                height = 10;
+                                width = 7;
+                                splashDamage = 40f;
+                                splashDamageRadius = 30f;
+                                backColor = Pal.bulletYellowBack;
+                                frontColor = Pal.bulletYellow;
+                                hitEffect = Fx.flakExplosion;
+                                collidesGround = true;
+                            }}
+                    );
+
+                    consumePower(3f);
+
+                    shoot = new ShootSpread();
+                    shootY = 3f;
+                    reload = 100f;
+                    range = 110;
+                    inaccuracy = 1f;
+                    shootCone = 0.1f;
+                    ammoUseEffect = Fx.casing1;
+                    health = 240;
+                    rotateSpeed = 6f;
+                    coolant = consumeCoolant(0.15f);
+                    researchCostMultiplier = 0.06f;
+
+                    limitRange();
+                }};
     }
 };}}
