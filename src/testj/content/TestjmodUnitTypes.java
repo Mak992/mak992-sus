19a823ca07428faa0117f63c8d060724787e5d50
Mak992 <maksimponomarev992@gmail.com>
2022-12-27 20:53:40 +0300

    test

---
+++ src/testj/content/TestjmodUnitTypes.java
@@ -0,0 +1,33 @@
+package testj.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.ai.*;
+import mindustry.ai.types.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.ammo.*;
+import mindustry.type.unit.*;
+import mindustry.type.weapons.*;
+import mindustry.world.meta.*;
+
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+
+public class TestjmodUnitTypes {
+
+    public static @EntityDef({Unitc.class, Carc.class}) UnitType rota;
+}
